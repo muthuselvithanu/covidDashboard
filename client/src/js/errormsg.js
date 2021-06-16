b1119a33ea3505fa664174fb232f99cf6d4f1be9
Muthuselvi Thanu <58234288+muthuselvithanu@users.noreply.github.com>
2021-06-16 19:12:30 -0400

    Delete errormsg.js

--- client/src/js/errormsg.js
+++
@@ -1,7 +0,0 @@
-const errorMessages = {
-    "email": "Please Enter Valid Email Id",
-    "password":"Please Enter Valid Password",
-    "username":"Please Enter Valid Username",
-    "zipcode":"Please Enter Valid Zipcode"
-};
-export{errorMessages};
